ff675895ce2d96662f3bc12a7b71e7b57c680e52
Adam Dickin <adam.dickin@amd.com>
2025-04-02 15:33:01 +0000

    fix if

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -76,7 +76,7 @@ def cmake_build(Map conf=[:]){
         setup_args = " -DBUILD_DEV=Off -DCMAKE_INSTALL_PREFIX=${miopen_install_path}" + setup_args
     } else if(package_build == true) {
         setup_args = ' -DBUILD_DEV=Off' + setup_args
-    } else if() {
+    } else if(coverage_build == true) {
         setup_args = ' -DBUILD_DEV=Off' + setup_args
     } else {
         setup_args = ' -DBUILD_DEV=On' + setup_args
